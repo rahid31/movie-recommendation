f003257ffe106c2e73a399b75daa252af9043ea1
Alvin Dihar <dihar.alvin31@gmail.com>
2025-01-29 18:39:49 +0700

    Add movie details

--- app.py
+++ app.py
@@ -19,26 +19,51 @@ def get_recommendation(title, cosine_sim=cosine_sim):
     return movies['title'].iloc[movies_indices]
 
 def fetch_poster(movie_id):
-    response = requests.get('https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}')
+    response = requests.get(f'https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}')
     data = response.json()
     poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500{poster_path}".format(poster_path)
+    full_path = f"https://image.tmdb.org/t/p/w500{poster_path}"
     return full_path
 
-st.title("Movie Recommendation System")
 
-selected_movie = st.selection("Select a movie:", movies['title'].values)
+# Streamlit UI
+st.title("Movie Recommendations")
 
-if st.button('Recommend'):
+# Select a movie
+selected_movie = st.selectbox("Select a movie:", movies['title'].values)
+
+# Display movie details
+if selected_movie:
+    movie_data = movies[movies['title'] == selected_movie].values[0]
+    poster_url = fetch_poster(movie_data[0])
+    movie_description = movie_data[-2]
+
+    # Display movie poster and description
+    with st.container():
+        cols = st.columns([1, 2])  # Left (1 part) | Right (2 parts)
+
+        with cols[0]:
+            st.image(poster_url, width=200)
+
+        with cols[1]:
+            st.text_area("Movie Description:", movie_description, height=270, disabled=True)
+
+    st.write("")
+    
+    st.write("")
+
+    # Display similar movies (Recommendation System)
+    st.write(f"Movies similar to {selected_movie}:")
     recommendations = get_recommendation(selected_movie)
-    st.write("Top 10 recommended movies:")
 
+    # Display movie posters
     for i in range(0, 10, 5):
         cols = st.columns(5)
         for col, j in zip(cols, range(i, i+5)):
-            movie_title = recommendations.iloc[j]['title']
-            movie_id = recommendations.iloc[j]['movie_id']
-            poster_url = fetch_poster(movie_id)
+            if j < len(recommendations):
+                movie_title = recommendations.iloc[j]
+                movie_id = movies.loc[movies['title'] == movie_title, 'movie_id'].values[0]
+                poster_url = fetch_poster(movie_id)
             
             with col:
                 st.image(poster_url, width=130)
